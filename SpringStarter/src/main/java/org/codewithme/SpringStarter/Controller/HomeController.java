746de95d0f8d3893bef3bd485445481bcfe78d1c
Seguya-Nicholus <seguyanicholus2015@gmail.com>
2024-11-08 13:21:14 +0300

    added templates and static files

---
+++ SpringStarter/src/main/java/org/codewithme/SpringStarter/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package org.codewithme.SpringStarter.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        return "home";
+    }
+}
